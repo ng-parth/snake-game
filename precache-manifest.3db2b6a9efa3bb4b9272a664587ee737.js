29996a86966328285f7fc4886e706a0cb48dcbeb
Rajat Srivastava <rajat.srivastava.20893@gmail.com>
2019-02-28 02:01:39 +0530

    Updates

---
+++ precache-manifest.3db2b6a9efa3bb4b9272a664587ee737.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d4699606036388f4309e",
+    "url": "/snake-game/static/js/runtime~main.d4699606.js"
+  },
+  {
+    "revision": "0071c53d731bce8cafb7",
+    "url": "/snake-game/static/js/main.0071c53d.chunk.js"
+  },
+  {
+    "revision": "ea2a8f4294c83d8852d5",
+    "url": "/snake-game/static/js/2.ea2a8f42.chunk.js"
+  },
+  {
+    "revision": "0071c53d731bce8cafb7",
+    "url": "/snake-game/static/css/main.6e7ebabf.chunk.css"
+  },
+  {
+    "revision": "761c37648267c97901e72c18fa95bc6a",
+    "url": "/snake-game/index.html"
+  }
+];
